ca6a72d90c2928f99626390b517cf7f283bd7643
AnibalAbrilDJ <anibalabril0324@gmail.com>
2024-04-19 21:54:09 -0500

    cracion de los componentes list'persona, agregar-editar-persona y creacion de services e interfaces persona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListPersonasComponent } from './components/list-personas/list-personas.component';
+import { AgregarEditarPersonaComponent } from './components/agregar-editar-persona/agregar-editar-persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPersonasComponent,
+    AgregarEditarPersonaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/interfaces/persona.ts
@@ -0,0 +1,10 @@
+export interface Persona{
+    id?: number;
+    cedula: string;
+    nombre: string;
+    apellido: string;
+    telefono: string;
+    direccion : string;
+    sexo : string;
+    fecha_nacimiento: Date;
+}
