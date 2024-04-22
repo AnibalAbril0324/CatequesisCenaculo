bff917e90dd0574811e14cbc8d48f9873b295a20
AnibalAbrilDJ <anibalabril0324@gmail.com>
2024-04-21 23:23:07 -0500

    Configuracion de la Tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//componentes
 import { AppComponent } from './app.component';
 import { ListPersonasComponent } from './components/list-personas/list-personas.component';
 import { AgregarEditarPersonaComponent } from './components/agregar-editar-persona/agregar-editar-persona.component';
+
+//angular materials
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,11 @@ import { AgregarEditarPersonaComponent } from './components/agregar-editar-perso
     AgregarEditarPersonaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatTableModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/list-personas/list-personas.component.ts
+++ src/app/components/list-personas/list-personas.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import { Persona } from 'src/app/interfaces/persona';
+
+const listpersonas: Persona[] = [
+  {cedula:"0105541510",nombre: "Anibal", apellido: "Abril", telefono: "0987654321", 
+    direccion : "Banos",sexo : "Masculino",fecha_nacimiento: new Date()},
+  {cedula:"0105541510",nombre: "Anibal", apellido: "Abril", telefono: "0987654321", 
+    direccion : "Banos",sexo : "Masculino",fecha_nacimiento: new Date()},
+  {cedula:"0105541510",nombre: "Anibal", apellido: "Abril", telefono: "0987654321", 
+    direccion : "Banos",sexo : "Masculino",fecha_nacimiento: new Date()},
+  {cedula:"0105541510",nombre: "Anibal", apellido: "Abril", telefono: "0987654321", 
+    direccion : "Banos",sexo : "Masculino",fecha_nacimiento: new Date()},
+  {cedula:"0105541510",nombre: "Anibal", apellido: "Abril", telefono: "0987654321", 
+    direccion : "Banos",sexo : "Masculino",fecha_nacimiento: new Date()},
+  {cedula:"0105541510",nombre: "Anibal", apellido: "Abril", telefono: "0987654321", 
+    direccion : "Banos",sexo : "Masculino",fecha_nacimiento: new Date()},
+  {cedula:"0105541510",nombre: "Anibal", apellido: "Abril", telefono: "0987654321", 
+    direccion : "Banos",sexo : "Masculino",fecha_nacimiento: new Date()},
+];
 
 @Component({
   selector: 'app-list-personas',
@@ -6,5 +24,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-personas.component.css']
 })
 export class ListPersonasComponent {
-
+  displayedColumns: string[] = ['Cedula', 'Nombre', 'Apellido', 'Telefono','Direccion','Sexo','fecha_Nacimiento'];
+  dataSource = listpersonas;
 }
